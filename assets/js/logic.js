e6d648c18280fac8c0d8bd05e3fe573cd9d84c60
Joey M <172998670+mankovich@users.noreply.github.com>
2024-07-04 16:04:44 -0400

    staggered update of progress on code as working through styling and getting into JavaScript

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,27 @@
 // TODO: define function for switching to dark mode and vice versa
 
 // TODO: create eventListener for clicking light-button
+
+// TODO: create eventListener for clicking submit-button?? Or will that just operate within the form? I don't recall
+// Either way also need to make it so that the user is moved to the blog page upon submission. 
+
+
+// /* The user's cursor is positioned over the element */
+// button:hover {
+//     background-color: #772014;
+//     color: #fff;
+//   }
+  
+//   /* The user is actively pressing down on the element */
+//   button:active {
+//     font-size: 180%;
+//     box-shadow: 0 0 10px #000 inset;
+//   }
+  
+//   /* The element is currently in focus, either by tabbing to or clicking on it */
+//   button:focus {
+//     outline: none;
+//     border-color: #8ac4ff;
+//   }
+
+// Access 
