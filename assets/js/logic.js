ad71177c0fc5ef573d9055107a05bc1f3589b675
Joey M <172998670+mankovich@users.noreply.github.com>
2024-07-05 00:14:08 -0400

    updates

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,28 +1,30 @@
-// TODO: define function for switching to dark mode and vice versa
+const usernameInput = document.querySelector("#username");
+const emailInput = document.querySelector("#email");
+const blogContentInput = document.querySelector("#blog-content");
+const msgDiv = document.querySelector("#error-msg");
 
-// TODO: create eventListener for clicking light-button
+function submitBlogPostForm() {
+    
+}
 
-// TODO: create eventListener for clicking submit-button?? Or will that just operate within the form? I don't recall
-// Either way also need to make it so that the user is moved to the blog page upon submission. 
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const username = usernameInput.value;
+    const email = emailInput.value;
+
+    if (username === "") {
+        displayMessage('error', 'Username cannot be blank');
+    } else if (email === "") {
+        displayMessage('error', )
+    }
+
+})
 
 
-// /* The user's cursor is positioned over the element */
-// button:hover {
-//     background-color: #772014;
-//     color: #fff;
-//   }
-  
-//   /* The user is actively pressing down on the element */
-//   button:active {
-//     font-size: 180%;
-//     box-shadow: 0 0 10px #000 inset;
-//   }
-  
-//   /* The element is currently in focus, either by tabbing to or clicking on it */
-//   button:focus {
-//     outline: none;
-//     border-color: #8ac4ff;
-//   }
+
+
+
 
 // Accessing HTML theme toggle element
 const themeSwitcher = document.querySelector('#theme-switcher');
@@ -42,3 +44,6 @@ themeSwitcher.addEventListener('click', function() {
         container.setAttribute('class', 'light');
     }
 });
+
+
+// TODO: save current theme mode in local storage
