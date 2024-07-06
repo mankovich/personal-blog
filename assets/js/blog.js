695bff461643e028c0e0cb755e2c7b2bebe61e95
Joey M <172998670+mankovich@users.noreply.github.com>
2024-07-06 02:44:51 -0400

    updated operational js with attributes and styling to come

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,6 +1,53 @@
-const backButton = document.querySelector("#back-button")
+// Selecting existing HTML elements
+const backButton = document.querySelector('#back-button')
+const postsContainerDiv = document.querySelector('#posts-container');
+// creating HTML elements
+const blogPostDiv = document.createElement('div');
+const titleEl = document.createElement('h4');
+const contentEl = document.createElement('p');
+const authorEl = document.createElement('p')
+
+blogPostDiv.setAttribute(
+    'style', 
+    'display: flex; '
+)
+
+titleEl.setAttribute(
+    'style',
+    ''
+)
+
+contentEl.setAttribute(
+    'style',
+    ''
+)
+
+authorEl.setAttribute(
+    'style',
+    ''
+)
+
+function renderBlogPost() { 
+    // retrieving the text input from local storage and parsing into javasript
+    const blogPost = JSON.parse(localStorage.getItem('blogPost'));
+    // checking if data is retrieved for early exit if not
+    if (blogPost !== null) {
+        titleEl.textContent = blogPost.title;
+        contentEl.textContent = blogPost.blogContent;
+        authorEl.textContent = blogPost.username;
+
+        blogPostDiv.append(titleEl, contentEl, authorEl);
+        postsContainerDiv.append(blogPostDiv);
+    }
+}
+
+function init() {
+    renderBlogPost();
+    console.log(localStorage.blogPost)
+}
+
+init();
 
 backButton.addEventListener('click', function() {
     location.assign('index.html')
 })
-
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,5 @@
 const usernameInput = document.querySelector('#username');
-const emailInput = document.querySelector('#email');
+const titleInput = document.querySelector('#title');
 const blogContentInput = document.querySelector('#blog-content');
 const msgDiv = document.querySelector('#msg');
 const submitButton = document.querySelector('#submit-button');
@@ -13,21 +13,22 @@ submitButton.addEventListener('click', function(event) {
     event.preventDefault();
     
     const username = usernameInput.value;
-    const email = emailInput.value;
+    const title = titleInput.value;
     const blogContent = blogContentInput.value;
     
     if (username === "") {
         displayMessage('**Username cannot be blank**');
-    } else if (email === "") {
-        displayMessage('**Email cannot be blank**');
+    } else if (title === "") {
+        displayMessage('**Title cannot be blank**');
     } else if (blogContent === "") {
         displayMessage('**Content cannot be blank**');
-    } else blogPosts = {
-        username: usernameInput.value,
-        email: emailInput.value,
-        blogContent: blogContentInput.value,
+    } else {
+        const blogPost = {
+            username: username,
+            title: title,
+            blogContent: blogContent,
+        }
+        localStorage.setItem('blogPost', JSON.stringify(blogPost))
+        location.assign("blog.html");
     }
-    
-    localStorage.setItem('blogPost', JSON.stringify(blogPosts));
-    location.assign("blog.html");
 })
